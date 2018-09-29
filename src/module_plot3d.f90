447a5c5a03246eb094161e3f79f2d077e3fcd3b2
Liu Jianxin <shookware@tju.edu.cn>
2018-09-29 15:08:17 +0100

    some modification

--- src/module_plot3d.f90
+++ src/module_plot3d.f90
@@ -91,6 +91,7 @@ contains
 
     this%grid=trim(fn)
     open(99, file=this%grid//'', form='unformatted')
+    print*, 'Reading Grid file', this%grid//''
     read(99)
     read(99)in, jn, kn
     call this%Create(in, jn, kn)
@@ -109,8 +110,9 @@ contains
     integer :: in, jn, kn, ln
     integer :: i, j, k, l
 
-    this%grid=trim(fn)
+    this%func=trim(fn)
     open(99, file=this%func//'', form='unformatted')
+    print*, 'Reading function file', this%func//''
     read(99)
     read(99)in, jn, kn, ln
     print*, 'in=', in
@@ -131,6 +133,10 @@ contains
     class(plot3d_type), intent(inout) :: this
 
     open(99, file=this%grid//'', form='unformatted')
+    print*, 'Writing grid file', this%Grid//''
+    print*, 'in=', this%in
+    print*, 'jn=', this%jn
+    print*, 'kn=', this%kn
     write(99)1
     write(99)this%in, this%jn, this%kn
     write(99)this%x, this%y, this%z
@@ -141,11 +147,18 @@ contains
   subroutine WriteFunc(this)
 
     class(plot3d_type), intent(inout) :: this
+    integer :: i, j, k, l
 
     open(99, file=this%func//'', form='unformatted')
+    print*, 'Writing function file', this%func//''
+    print*, 'in=', this%in
+    print*, 'jn=', this%jn
+    print*, 'kn=', this%kn
+    print*, 'ln=', this%ln
     write(99)1
     write(99)this%in, this%jn, this%kn, this%ln
-    write(99)this%q
+    write(99)((((this%q(i, j, k, l), i=1, this%in), j=1, this%jn), k=1, this%kn), &
+                l=1, this%ln)
     close(99)
 
   end subroutine WriteFunc
